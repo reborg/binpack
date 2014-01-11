797db933474933c226b77ebef162093ec3a4c7d3
Renzo Borgatti <reborg@reborg.net>
2014-01-11 22:25:28 +0000

    Packing with constraint on container size

--- src/knapsack/core.clj
+++ src/knapsack/core.clj
@@ -1,14 +1,49 @@
 (ns knapsack.core)
 
-(defn resize [coll n]
-  "Grow coll up to n items using nil fillers"
-  (concat coll (take (- n (count coll)) (repeat nil))))
+(defn resize [items n]
+  "Grow items up to n items using nil fillers"
+  (concat items (take (- n (count items)) (repeat nil))))
 
-(defn unconstrained-packing [n items]
-  "Packs items into n containers, bigger items first, without
-  limits to the size of packed items in each container."
-  (loop [xs (reverse (sort items)) containers (repeat n (list))]
+(defn exceeded? [containers sizes]
+  "True if any of the containers contain more than
+  the allowed size."
+  (not (every? true? (map #(<= %1 %2) (map (partial reduce +) containers) sizes))))
+
+(defn index-of [item coll]
+  "Retrieve the index of the first occurence of item in coll"
+  (let [res (count (take-while #(not= item %) coll))]
+    (if (= res (count coll)) -1 res)))
+
+(defn add-first [item containers sizes]
+  "Attempt to allocate the item to the first available container."
+  (let [avail (map #(<= (reduce + item %1) %2) containers sizes)
+        i (index-of true avail)]
+    (if (neg? i)
+      containers
+      (assoc (vec containers) i (conj (nth containers i) item)))))
+
+(defn free-alloc [items containers sizes]
+  "Allocate items into containers without constraints. Sizes
+  are ignored."
+  (let [resized (resize items (count containers))]
+    (map (partial remove nil?) (map #(conj %2 %1) resized containers))))
+
+(defn size-alloc [items containers sizes]
+  "Allocate items into containers until size of the container is reached.
+  Will attempt allocating items in other containers if room available."
+  (let [free-allocation (free-alloc items containers sizes)]
+    (if (exceeded? free-allocation sizes)
+      (loop [[item & others] items cs containers]
+        (if (nil? item)
+          cs
+          (recur others (add-first item cs sizes))))
+      free-allocation)))
+
+(defn pack [items sizes f]
+  "Pack items into (count sizes) containers giving priority to the first items. 
+  Invoke strategy f to allocate new items into containers."
+  (loop [xs items cs (repeat (count sizes) (list))]
     (if (= 0 (count xs))
-      (->> containers (map (partial remove nil?)))
-      (let [[allocable others] (split-at (count containers) xs)]
-        (recur others (map #(conj %2 %1) (resize allocable (count containers)) containers))))))
+      cs
+      (let [[x1 x2] (split-at (count cs) xs)]
+        (recur x2 (f x1 cs sizes))))))
--- test/knapsack/core_test.clj
+++ test/knapsack/core_test.clj
@@ -2,12 +2,44 @@
   (:use [midje.sweet])
   (:require [knapsack.core :as k]))
 
+(fact "verification on size of containers"
+      (k/exceeded? [[3 2 3] [3 7]] [10 10]) => falsey
+      (k/exceeded? [[3 2 3] [3 5]] [10 1]) => truthy)
+
+(facts "conditional allocation"
+       (fact "room to allocate"
+             (k/add-first 3 [[1 2 3] [1 1]] [7 6]) => [[1 2 3] [1 1 3]])
+       (fact "should not allocate twice"
+             (k/add-first 3 [[1 3] [1 1]] [7 6]) => [[1 3 3] [1 1]]))
+
+(facts "allocation strategies"
+       (fact "unconstrained allocation"
+             (k/free-alloc [1 2 3] [[] [] []] nil) => [[1] [2] [3]]
+             (k/free-alloc [1 2] [[] [] []] nil) => [[1] [2] []]
+             (k/free-alloc [1 2] [[4] [6]] nil) => [[4 1] [6 2]]
+             (k/free-alloc [1 2 3] [[] []] nil) => [[1] [2]])
+       (fact "container size restricted allocation"
+             (fact "all full, nothing to do"
+                   (k/size-alloc [1 2 3] [[5] [5] [5]] [5 5 5]) => [[5] [5] [5]])
+             (fact "there is room somewhere"
+                   (k/size-alloc [5 5 1] [[1] [5] [5]] [5 5 5]) => [[1 1] [5] [5]])))
+
 (facts "unconstrained packing of items in containers"
        (fact "even distribution"
-             (k/unconstrained-packing 2 [2 2 1 1]) => [[1 2] [1 2]])
+             (k/pack [2 2 1 1] [0 0] k/free-alloc) => [[1 2] [1 2]])
        (fact "single container"
-             (k/unconstrained-packing 1 (range 10)) => [(range 10)])
+             (k/pack (reverse (range 10)) [0] k/free-alloc) => [(range 10)])
        (fact "asymmetric packing"
-             (k/unconstrained-packing 3 [2 3 1 1 4 2 1]) => [[1 2 4] [1 3] [1 2]])
+             (k/pack (reverse (sort [2 3 1 1 4 2 1])) [0 0 0] k/free-alloc) => [[1 2 4] [1 3] [1 2]])
        (fact "asymmetric packing"
-             (k/unconstrained-packing 3 (range 10)) => [[0 3 6 9] [2 5 8] [1 4 7]]))
+             (k/pack (reverse (range 10)) [0 0 0] k/free-alloc) => [[0 3 6 9] [2 5 8] [1 4 7]]))
+
+(facts "pack items without exceeding container sizes"
+       (fact "no space whatsoever"
+             (k/pack [2 2 1 1] [0 0] k/size-alloc) => [[] []])
+       (fact "single container"
+             (k/pack (range 1 10) [10] k/size-alloc) => [[4 3 2 1]])
+       (fact "asymmetric packing"
+             (k/pack [10 20 30 40 50 60 70] [100 20 80] k/size-alloc) => [[50 40 10] [20] [30]])
+       (fact "big items take precedence when reversed"
+             (k/pack (reverse (range 1 100)) (take 5 (repeat 50)) k/size-alloc) => [[50] [1 49] [2 48] [3 47] [4 46]]))
